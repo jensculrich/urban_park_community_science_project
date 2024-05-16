82c549a5a78da38f6a520cf701e814db2ea46278
jensculrich <jensj27@gmail.com>
2024-05-16 09:48:48 -0700

    rename/reorganized data files, downloaded full iNat dataset for los angeles county

--- run_model/prep_data.R
+++ run_model/prep_data.R
@@ -8,11 +8,11 @@ prep_data <- function(grid_size,
   
   # first read the data 
   df <- rbind(
-    cbind(read.csv("./data/city-nature-challenge-2020-los-angeles-county.csv"), year = 1),
-    cbind(read.csv("./data/city-nature-challenge-2021-los-angeles-county.csv"), year = 2),
-    cbind(read.csv("./data/city-nature-challenge-2022-los-angeles-county.csv"), year = 3),
-    cbind(read.csv("./data/city-nature-challenge-2023-los-angeles-county.csv"), year = 4),
-    cbind(read.csv("./data/city-nature-challenge-2024-los-angeles-county.csv"), year = 5)
+    cbind(read.csv("./data/city_nature_challenge_2020_2024_los_angeles_county/city-nature-challenge-2020-los-angeles-county.csv"), year = 1),
+    cbind(read.csv("./data/city_nature_challenge_2020_2024_los_angeles_county/city-nature-challenge-2021-los-angeles-county.csv"), year = 2),
+    cbind(read.csv("./data/city_nature_challenge_2020_2024_los_angeles_county/city-nature-challenge-2022-los-angeles-county.csv"), year = 3),
+    cbind(read.csv("./data/city_nature_challenge_2020_2024_los_angeles_county/city-nature-challenge-2023-los-angeles-county.csv"), year = 4),
+    cbind(read.csv("./data/city_nature_challenge_2020_2024_los_angeles_county/city-nature-challenge-2024-los-angeles-county.csv"), year = 5)
   )
           
   # and perform some initial filters
@@ -38,7 +38,7 @@ prep_data <- function(grid_size,
   #-----------------------------------------------------
   # map the data on a spatial file
   
-  county_shp <- read_sf("./data/Los_Angeles_County_Boundary/County_Boundary.SHP")
+  county_shp <- read_sf("./data/los_angeles_county_boundary_shapefile/County_Boundary.SHP")
   
   # USA_Contiguous_Albers_Equal_Area_Conic
   crs <- 5070
